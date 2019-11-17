6ea418e427b34128d53c5a0a9bc7a360e45c9f8c
Paul Kelly <paulkelly2@tudublin.ie>
2019-11-17 21:58:41 +0000

    functions

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,11 @@ module Main exposing (..)
 import Html exposing (Html, span, text)
 import Html.Attributes exposing (class)
 
+alwaysReturn42 =
+    21 + 21
 
 main : Html a
 main =
-    span [ class "welcome-message" ] [ text "Hello, World!" ]
+    let message = "" ++ String.fromInt alwaysReturn42
+    in span [ class "welcome-message" ] [ text message ]
+
