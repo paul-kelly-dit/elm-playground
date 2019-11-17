f532526366c1d3b7a8780d1a92456c22224180d7
Paul Kelly <paulkelly2@tudublin.ie>
2019-11-17 22:14:14 +0000

    ui updated

--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,42 @@
 module Main exposing (..)
 
-import Html exposing (Html, span, text)
-import Html.Attributes exposing (class)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
-
-
-
-type Pizza
-    = Calzone
-    | Margherita
-    | QuattroStagione
-
-
-getPizzaTupleAsString : ( Pizza, Pizza ) -> String
-getPizzaTupleAsString t =
-    let
-        ( pz1, pz2 ) =
-            t
-    in
-    ">>>> OUTPUT >>>> " ++ Debug.toString pz1 ++ "," ++ Debug.toString pz2
-
-
-main : Html a
+-- MAIN
 main =
-    let
-        message = getPizzaTupleAsString (Calzone, QuattroStagione)
-    in
-    span [ class "welcome-message" ] [ text message ]
+    Browser.sandbox { init = init, update = update, view = view }
+
+-- MODEL
+type alias Model =
+    Int
+
+init : Model
+init =
+    0
+
+-- UPDATE
+type Msg
+    = Increment
+    | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
